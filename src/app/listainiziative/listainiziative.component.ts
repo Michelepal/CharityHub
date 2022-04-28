16e235c35dbf2f658ffafd58e161f3729dd66d1a
Michelepal <Mikaelquasimodus@gmail.com>
2022-04-28 08:39:21 +0200

    fix bottone filtro

--- src/app/listainiziative/listainiziative.component.ts
+++ src/app/listainiziative/listainiziative.component.ts
@@ -3,6 +3,7 @@ import {HttpClient} from '@angular/common/http';
 import { Association } from '../model/association';
 import { environment } from 'src/environments/environment';
 import { Iniziative } from '../model/iniziative';
+import { Regioni } from '../model/regioni';
 
 @Component({
   selector: 'app-listainiziative',
@@ -12,13 +13,18 @@ import { Iniziative } from '../model/iniziative';
 export class ListainiziativeComponent  {
   associazioni: Association[] = [];
   iniziative: Iniziative[] = [];
+  regioni: Regioni[] = [];
 
   constructor(private http: HttpClient) {
     this.http.get<Association[]>(environment.baseUrl + '/associazioni').subscribe(data => this.associazioni = data);
     this.http.get<Iniziative[]>(environment.baseUrl + '/iniziative').subscribe(data => this.iniziative = data);
+    this.http.get<Regioni[]>(environment.baseUrl + '/regioni').subscribe(data => this.regioni = data);
+
   }
 
   filtroIniziative(filtro: string) {
+    const baseUrl = environment.baseUrl + '/iniziative';
+    this.http.get<Iniziative[]>(`${baseUrl}?reg=${filtro}`).subscribe(data => this.iniziative = data);
 
   }
 
