b9eab72a81f2ba4c4ff512f9964cf46b6203518a
Michelepal <Mikaelquasimodus@gmail.com>
2022-05-02 18:59:45 +0200

    fix

--- src/app/listainiziative/listainiziative.component.ts
+++ src/app/listainiziative/listainiziative.component.ts
@@ -19,7 +19,6 @@ export class ListainiziativeComponent  {
     this.http.get<Association[]>(environment.baseUrl + '/associazioni').subscribe(data => this.associazioni = data);
     this.http.get<Iniziative[]>(environment.baseUrl + '/iniziative').subscribe(data => this.iniziative = data);
     this.http.get<Regioni[]>(environment.baseUrl + '/regioni').subscribe(data => this.regioni = data);
-
   }
 
   filtroIniziative(filtro: string) {
